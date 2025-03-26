65f89c33d234dd3f68fd79567e032d26abc61535
sashabrava <sashabrava0@gmail.com>
2025-03-26 09:59:21 +0100

    T-2 Fix nginx issue

--- 3-instances.tf
+++ 3-instances.tf
@@ -5,76 +5,27 @@ resource "aws_instance" "ubuntu_instance" {
   key_name               = "MyKeyPair"
   subnet_id              = aws_subnet.main_subnet.id
   vpc_security_group_ids = [aws_security_group.ubuntu_sg.id]
-  associate_public_ip_address = true # Public IP for internet access
+  # associate_public_ip_address = true # Public IP for internet access
   user_data_replace_on_change = true
 
   user_data = <<-EOF
               #!/bin/bash
               apt-get update -y
-              apt-get install -y nginx ca-certificates curl fcgiwrap
-
-              systemctl enable fcgiwrap
-              systemctl start fcgiwrap
-              
-              cat > /var/www/html/lsb_release.cgi <<'LSB'
-              #!/bin/bash
-              echo "Content-type: text/plain"
-              echo ""
-              lsb_release -a
-              LSB
-
-              chmod +x /var/www/html/lsb_release.cgi
-
-              # Configure nginx
-              cat > /var/www/html/index.nginx-debian.html <<'INDEX'
-              <html>
-               <body>
-                      <h1>Hello World!</h1>
-                      <div class="info">
-                        <!--#include virtual="/lsb_release.cgi" -->
-                      </div>
-                  </div>
-              </body>
-              </html>
-              INDEX
-
-             cat > /etc/nginx/sites-available/default <<'NGINX_CONFIG'
-             server {
-                listen 80 default_server;
-                listen [::]:80 default_server;
-
-                root /var/www/html;
-
-                # Add index.php to the list if you are using PHP
-                index index.html index.htm index.nginx-debian.html;
-
-                server_name _;
-
-                location / {
-                # First attempt to serve request as file, then
-                # as directory, then fall back to displaying a 404.
-                ssi on;
-                try_files $uri $uri/ =404;
-                }
-                location ~ \.cgi$ {
-                    root /var/www/html;
-                    fastcgi_pass unix:/var/run/fcgiwrap.socket;
-                    include fastcgi_params;
-                    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-                }
-                }
-                NGINX_CONFIG
-
+              apt-get install -y nginx
+              echo "<html><body><h1>Hello World</h1> <div>$(lsb_release -d | cut -f2)</div></body></html>" > /var/www/html/index.html
               systemctl enable nginx
               systemctl start nginx
+
+              # Docker Installation (Official Guide)
+              apt-get install -y ca-certificates curl gnupg
               install -m 0755 -d /etc/apt/keyrings
-              curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-              chmod a+r /etc/apt/keyrings/docker.asc
-              echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-                            noble stable" | \
-              tee /etc/apt/sources.list.d/docker.list > /dev/null
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/keyrings/docker.gpg > /dev/null
+              chmod a+r /etc/apt/keyrings/docker.gpg
+              echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
               apt-get update -y
-              apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+              apt-get install -y docker-ce docker-ce-cli containerd.io
+              systemctl enable docker
+              systemctl start docker
               EOF
 
   tags = {
